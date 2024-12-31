b87a0866cc731229e5eea93ac63cda7aadffb193
toha <samsuddintoha@gmail.com>
2024-12-31 21:45:53 +0600

    Added Escape for ending game

--- js/gameScript.js
+++ js/gameScript.js
@@ -3,29 +3,31 @@ document.addEventListener('keyup', (event) => {
   const currentAlphabetElement = document.getElementById('current-alphabet');
   const currentAlphabet = currentAlphabetElement.innerText;
 
+  if (playerPressedKey === 'Escape') {
+    endGame();
+  }
+
   if (playerPressedKey === currentAlphabet) {
     removeKeyboardBackgroundColorById(currentAlphabet);
-    const score = getElementIntValueById('current-score')
+    const score = getElementIntValueById('current-score');
     const newScore = score + 1;
-    setElementValueById('current-score', newScore)
-    
+    setElementValueById('current-score', newScore);
+
     continueGame();
-    removeClassById('current-alphabet', 'shake')
-    removeClassById('current-alphabet', 'text-red-600')
-
+    removeClassById('current-alphabet', 'shake');
+    removeClassById('current-alphabet', 'text-red-600');
   } else {
-    const life = getElementIntValueById('current-life')
-    
-    const existLife = life - 1;
-    setElementValueById('current-life', existLife)
+    const life = getElementIntValueById('current-life');
 
-    if(existLife){
-      addClassById('current-alphabet', 'shake')
-      addClassById('current-alphabet', 'text-red-600')
+    const existLife = life - 1;
+    setElementValueById('current-life', existLife);
+
+    if (existLife) {
+      addClassById('current-alphabet', 'shake');
+      addClassById('current-alphabet', 'text-red-600');
     }
     if (existLife === 0) {
       endGame();
-      removeKeyboardBackgroundColorById(currentAlphabet)
     }
   }
 });
@@ -34,15 +36,16 @@ function endGame() {
   hideElementById('playground');
   showElementById('score');
 
-  const score = getElementIntValueById('current-score')
+  const currentAlphabet = document.getElementById('current-alphabet').innerText;
+  removeKeyboardBackgroundColorById(currentAlphabet);
+
+  const score = getElementIntValueById('current-score');
   setElementValueById('total-score', score);
 }
 
 function continueGame() {
   const alphabet = getARandomAlphabet();
-
   document.getElementById('current-alphabet').innerText = alphabet;
-
   setKeyboardBackgroundColorById(alphabet);
 }
 
@@ -54,8 +57,8 @@ function play() {
   setElementValueById('current-score', 0);
   setElementValueById('current-life', 5);
 
-  removeClassById('current-alphabet', 'shake')
-  removeClassById('current-alphabet', 'text-red-600')
+  removeClassById('current-alphabet', 'shake');
+  removeClassById('current-alphabet', 'text-red-600');
 
   continueGame();
 }
